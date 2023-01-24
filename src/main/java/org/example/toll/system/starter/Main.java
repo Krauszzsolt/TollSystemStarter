bcfe8038846d6f19e65041d589199de73861d996
krauszzsolt <krauszzsolt@lechnerkozpont.hu>
2023-01-24 17:36:05 +0100

    Stage 2 WIP

--- src/main/java/org/example/toll/system/starter/Main.java
+++ src/main/java/org/example/toll/system/starter/Main.java
@@ -9,20 +9,22 @@ import org.example.toll.system.consol.ui.TollSystemController;
 import org.example.toll.system.consol.ui.TollSystemPresenterImpl;
 import org.example.toll.system.consol.ui.TollSystemView;
 import org.example.toll.system.persistence.storage.TollSystemSqlitePersistenceStorage;
+import org.example.vehicle.register.plugin.VehicleRegisterPlugin;
 
 public class Main {
     static VehicleRegisterPersistenceStorage fileStorage = new VehicleRegisterFileStorage();
     static TollSystempersistenceStorage tollSystempersistenceStorage = new TollSystemSqlitePersistenceStorage();
     static TollSystemPresenterImpl tollSystemPresenter = new TollSystemPresenterImpl();
-    static TollSystemApp tollSystemApp = new TollSystemApp(tollSystempersistenceStorage, tollSystemPresenter);
-    static VehicleRegister vehicleRegister = new VehicleRegister(tollSystemApp, fileStorage);
+    static VehicleRegisterPlugin vehicleRegisterPlugin = new VehicleRegisterPlugin();
+    static TollSystemApp tollSystemApp = new TollSystemApp(tollSystempersistenceStorage, tollSystemPresenter, vehicleRegisterPlugin);
+    static VehicleRegister vehicleRegisterApp = new VehicleRegister(vehicleRegisterPlugin, fileStorage);
     static TollSystemController tollSystemController = new TollSystemController(tollSystemApp);
     static TollSystemView tollSystemView = new TollSystemView(tollSystemController);
 
 
     public static void main(String[] args) {
         tollSystemPresenter.setTollSystemView(tollSystemView);
-        tollSystemApp.setVehicleRegister(vehicleRegister);
+        vehicleRegisterPlugin.setVehicleRegister(vehicleRegisterApp);
         tollSystemView.run();
     }
 }
